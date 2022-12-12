2d2f8d8cc92ae2f63f0dcc71c539396d78117aea
Luiz Gabriel <luizgabriel1233@gmail.com>
2022-12-12 17:35:43 -0300

    change stages

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
 
 const Question = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
-    <div>Question</div>
+    <div id='question'>
+        <p>Pergunta 0 de 10</p>
+        <h2>Pergunta atual</h2>
+        <div id="options-container">
+            <p>Opções</p>
+        </div>
+        <button>Continuar</button>
+    </div>
   )
 }
 
